754e777854b39a76f2eacfdc4f40d10fad7991a0
Luke <luke@zxcv.net.au>
2016-05-25 11:03:14 +1000

    Set up game, style game board and set up pseudocode in .js file
    Signed-off-by: Luke <luke@zxcv.net.au>

---
+++ js/tictactoe.js
@@ -0,0 +1,23 @@
+// tictactoe.js
+
+// Randomise choice of player or computer to go first.
+
+// Players turn
+// Allow player to click on only one button on the gameboard.
+
+
+// On click change button to disabled="disabled" and add .crosses
+
+
+// Check for win by checking to see if three buttons in a line have the same
+// class of either .crosses or .noughts
+// If .crosses player wins, end game and print You win, display reset button
+// if .noughts computer wins, end game and print You lose, display reset button
+
+
+// Check for stalemate by checking if all buttons are disabled but no wins
+// If stalemate end game and print Stalemate, display reset button
+
+
+// If no win or stalemate, change to Computers turn
+// Randomise button selection from available buttons.
