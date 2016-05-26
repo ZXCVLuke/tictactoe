c1a86e86b842430ac790ab6da37709983df8b12d
Luke <luke@zxcv.net.au>
2016-05-26 17:01:36 +1000

    Added the reset button, got that to turn up when there is a win or stalemate. Also set up the computers turn

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -2,32 +2,81 @@
 
 // Randomise choice of player or computer to go first.
 
-// Players turn
-
-
-
-// On click change button to disabled="disabled" and add .crosses
-
 $( document ).ready(function() {
 
-    $( "button" ).click(function(){
-      $(this).addClass( "crosses" ).attr("disabled", "disabled");
+  // create an empty array of 9 elements for the 9 IDs.
+  var gameBoard = new Array(9);
+
+  $( ".board-tile" ).click(function(){
+
+    // On click change button to disabled="disabled" and add .crosses and name="crosses"
+    $(this).addClass( "crosses" ).attr("disabled", "disabled").attr("name", "crosses");
+
+    // check each id (ie. a0) for name of .noughts or .crosses
+    gameBoard[0] = $('#a0').attr('name');
+    gameBoard[1] = $('#a1').attr('name');
+    gameBoard[2] = $('#a2').attr('name');
+    gameBoard[3] = $('#b0').attr('name');
+    gameBoard[4] = $('#b1').attr('name');
+    gameBoard[5] = $('#b2').attr('name');
+    gameBoard[6] = $('#c0').attr('name');
+    gameBoard[7] = $('#c1').attr('name');
+    gameBoard[8] = $('#c2').attr('name');
+
+    // Check for win
+    if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2] && gameBoard[0] === "crosses" ||
+        gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[3] === "crosses" ||
+        gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8] && gameBoard[6] === "crosses" ||
+        gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6] && gameBoard[0] === "crosses" ||
+        gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7] && gameBoard[1] === "crosses" ||
+        gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8] && gameBoard[2] === "crosses" ||
+        gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8] && gameBoard[0] === "crosses" ||
+        gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6] && gameBoard[2] === "crosses") {
+      $(".board-tile").attr("disabled", "disabled");
+      $(".main").text("Game Over");
+      $(".sub").text("You win");
+      $(".reset").removeClass("hidden");
+    }
+
+    // Check for stalemate
+    else if (!!gameBoard[0] === true &&
+             !!gameBoard[1] === true &&
+             !!gameBoard[2] === true &&
+             !!gameBoard[3] === true &&
+             !!gameBoard[4] === true &&
+             !!gameBoard[5] === true &&
+             !!gameBoard[6] === true &&
+             !!gameBoard[7] === true &&
+             !!gameBoard[8] === true) {
+           $(".main").text("Game Over");
+           $(".sub").text("Stalemate, nobody wins");
+           $(".reset").removeClass("hidden");
+         }
+
+    else {
+      $(".board-tile").attr("disabled", "disabled");
+      $(".main").text("Computer's turn");
+      $(".sub").text("Hold tight");
+
+      // Randomly select tile, change class and name
+
+
+
+      // Check for win
+
+      // Check for stalemate
+
+      // Remove disabled attribute from buttons without names
+    }
+
+  }); // End move click function
+
+// Reset button
+    $( ".reset" ).click(function(){
+      $(".board-tile").removeClass( "crosses" ).removeAttr("disabled").removeAttr("name");
+      $(this).addClass("hidden");
+      $(".main").text("Your turn");
+      $(".sub").text("Choose a tile");
     });
 
 });
-
-// Allow player to click on only one button on the gameboard.
-
-
-// Check for win by checking to see if three buttons in a line have the same
-// class of either .crosses or .noughts
-// If .crosses player wins, end game and print You win, display reset button
-// if .noughts computer wins, end game and print You lose, display reset button
-
-
-// Check for stalemate by checking if all buttons are disabled but no wins
-// If stalemate end game and print Stalemate, display reset button
-
-
-// If no win or stalemate, change to Computers turn
-// Randomise button selection from available buttons.
